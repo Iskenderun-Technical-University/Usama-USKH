f901abae09b7145156f6fed4d559e7592b001c5f
usama67261 <osama.mhaleam@gmail.com>
2023-04-17 01:14:07 +0300

    Adding Some Features For Login Form

--- Project-iste/Project-iste/Form1.cs
+++ Project-iste/Project-iste/Form1.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Project_iste
 {
@@ -21,6 +23,14 @@ namespace Project_iste
             InitializeComponent();
         }
 
+
+        public static bool check(string str)
+        {
+            return (String.IsNullOrEmpty(str) || 
+                str.Trim().Length == 0) ? true : false;
+        }
+
+
         private void guna2ControlBox1_Click(object sender, EventArgs e)
         {
             Application.Exit();
@@ -32,43 +42,115 @@ namespace Project_iste
             string password = guna2TextBox2.Text;
             userid = guna2TextBox1.Text;
             com = new SqlCommand();
-            con.Open();
-            com.Connection = con;
-            com.CommandText = "Select * from Login where Username='" + guna2TextBox1.Text +
-                "'And password='" + guna2TextBox2.Text + "'";
-            SqlDataReader reader = com.ExecuteReader();
-            if (reader.Read())
-            {
-                this.Hide();
-                Account account = new Account();
-                account.Show();
-                account.label2.Text = userid;
-            }
-            else
-            {
-                MessageBox.Show("Invaild Login Details!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                guna2TextBox1.Clear(); guna2TextBox2.Clear(); guna2TextBox1.Focus();
-            }
-            con.Close();
-        }
-
-        private void guna2Button2_Click(object sender, EventArgs e)
-        {
-            if (guna2TextBox1.Text == "" || guna2TextBox2.Text == "" || guna2TextBox1.Text == " " || guna2TextBox1.Text == "  " || guna2TextBox1.Text == "   " || guna2TextBox2.Text == " " || guna2TextBox2.Text == "  " || guna2TextBox2.Text == "   ")
+            if (check(guna2TextBox1.Text) == true || check(guna2TextBox2.Text) == true)
             {
                 MessageBox.Show("please enter the required data", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                guna2TextBox1.Clear(); guna2TextBox2.Clear(); guna2TextBox1.Focus();
             }
             else
             {
                 con.Open();
-                string query = "INSERT INTO Login (UserName , Password) VALUES(@UserName,@Password) ";
-                SqlCommand cmd = new SqlCommand(query, con);
-                cmd.Parameters.AddWithValue("@UserName", guna2TextBox1.Text);
-                cmd.Parameters.AddWithValue("@Password", guna2TextBox2.Text);
-                cmd.ExecuteNonQuery();
+                com.Connection = con;
+                com.CommandText = "Select * from Login where Username='" + guna2TextBox1.Text +
+                    "'And password='" + guna2TextBox2.Text + "'";
+                SqlDataReader reader = com.ExecuteReader();
+                if (reader.Read())
+                {
+                    this.Hide();
+                    Account account = new Account();
+                    account.Show();
+                    account.label2.Text = userid;
+                }
+                else
+                {
+                    MessageBox.Show("Invaild Login Details!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    guna2TextBox1.Clear(); guna2TextBox2.Clear(); guna2TextBox1.Focus();
+                }
                 con.Close();
+            }
+        }
+
+        public static int passwordchar(string password)
+        {
+            int passwordchar = password.Length;
+            return passwordchar;
+        }
+
+        public static int passworsupper(string password)
+        {
+            int value = password.Length - Regex.Replace(password, "[A-Z]", "").Length;
+            return value;
+        }
+
+        public static int passworslower(string password)
+        {
+            int value = password.Length - Regex.Replace(password, "[a-z]", "").Length;
+            return value;
+        }
+
+        public static int passworsnumber(string password)
+        {
+            int value = password.Length - Regex.Replace(password, "[0-9]", "").Length;
+            return value;
+        }
+
+        public static int passworsspecial(string password)
+        {
+            int value = password.Length - Regex.Replace(password, "[!-/:-@]", "").Length;
+            return value;
+        }
+
+    
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            if (check(guna2TextBox1.Text) == true || check(guna2TextBox2.Text )== true)
+            {
+                MessageBox.Show("please enter the required data", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 guna2TextBox1.Clear(); guna2TextBox2.Clear(); guna2TextBox1.Focus();
-                MessageBox.Show("user saved");
+            }
+            else
+            {
+                if (passwordchar(guna2TextBox2.Text) < 8 || passwordchar(guna2TextBox2.Text)>=16)
+                {
+                    MessageBox.Show("your password must be 8-16 characters", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    guna2TextBox2.Clear(); guna2TextBox2.Focus();
+                }
+                else if (passworsupper(guna2TextBox2.Text) < 1)
+                {
+                    MessageBox.Show("your password must have at least one capital", "eror", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    guna2TextBox2.Clear(); guna2TextBox2.Focus();
+                }
+                else if (passworslower(guna2TextBox2.Text) < 1)
+                {
+                    MessageBox.Show("your password must have at least one lower case letter", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    guna2TextBox2.Clear(); guna2TextBox2.Focus();
+
+                }
+                else if (passworsnumber(guna2TextBox2.Text) < 1)
+                {
+                    MessageBox.Show("your password must have at least one number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    guna2TextBox2.Clear(); guna2TextBox2.Focus();
+
+                }
+                else if (passworsspecial(guna2TextBox2.Text) < 1)
+                {
+                    MessageBox.Show("your password must have at least one Special characters ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    guna2TextBox2.Clear(); guna2TextBox2.Focus();
+
+                }
+                else
+                {
+                    con.Open();
+                    string query = "INSERT INTO Login (UserName , Password) VALUES(@UserName,@Password) ";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@UserName", guna2TextBox1.Text);
+                    cmd.Parameters.AddWithValue("@Password", guna2TextBox2.Text);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    guna2TextBox1.Clear(); guna2TextBox2.Clear(); guna2TextBox1.Focus();
+                    MessageBox.Show("user saved");
+                }
 
             }
         }
